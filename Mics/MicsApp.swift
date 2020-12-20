2fb74b119553713a0b1437fc10fdefd1067504be
Christopher Hicks <infinitychristopher@gmail.com>
2020-12-19 17:38:39 -0800

    Initial Commit

---
+++ Mics/MicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicsApp.swift
+//  Mics
+//
+//  Created by Christopher Hicks on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct MicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
